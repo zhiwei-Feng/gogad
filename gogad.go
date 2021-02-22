8c77365f2d0c883b70e60e8833580180918584d7
fengzw <zhiwei.feng1995@gmail.com>
2021-02-22 19:54:55 +0800

    完成功能

--- gogad.go
+++ gogad.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -44,41 +45,53 @@ func readCSVToMachine(path string) []Machine {
 }
 
 func main() {
-	servers := readCSVToMachine("machines.csv")
-	port := 22
-	for _, server := range servers {
-		// ssh login
-		config := &ssh.ClientConfig{
-			Timeout:         5 * time.Second,
-			User:            server.User,
-			HostKeyCallback: ssh.InsecureIgnoreHostKey(), //这个可以， 但是不够安全
-		}
-		config.Auth = []ssh.AuthMethod{ssh.Password(server.Password)}
-
-		// build dial
-		addr := fmt.Sprintf("%s:%d", server.Host, port)
-		sshClient, err := ssh.Dial("tcp", addr, config)
-		if err != nil {
-			log.Printf("server %s connect fail\n", server.Host)
-			continue
-		}
-		defer sshClient.Close()
-
-		// create ssh session
-		session, err := sshClient.NewSession()
-		if err != nil {
-			log.Printf("server %s connect fail\n", server.Host)
-			continue
-		}
-		defer session.Close()
-
-		// exec nvidia-smi to get information of GPU
-		combo, err := session.CombinedOutput("nvidia-smi --query-gpu=memory.used,memory.total --format=csv,noheader")
-		if err != nil {
-			log.Printf("server %s exec nvidia-smi fail\n", server.Host)
-			continue
-		}
-
-		fmt.Printf("machine:[%s]->memory used, memory total\n%s", server.Host, string(combo))
+	if len(os.Args) == 1 {
+		fmt.Println("Please provide a config file path (csv file), e.g. \"machine-template.csv\"")
+		return
 	}
+	servers := readCSVToMachine(os.Args[1])
+	port := 22
+	var wg sync.WaitGroup
+	for _, server := range servers {
+		wg.Add(1)
+		go func(server Machine, port int) {
+			// ssh login
+			config := &ssh.ClientConfig{
+				Timeout:         5 * time.Second,
+				User:            server.User,
+				HostKeyCallback: ssh.InsecureIgnoreHostKey(), //这个可以， 但是不够安全
+			}
+			config.Auth = []ssh.AuthMethod{ssh.Password(server.Password)}
+
+			// build dial
+			addr := fmt.Sprintf("%s:%d", server.Host, port)
+			sshClient, err := ssh.Dial("tcp", addr, config)
+			if err != nil {
+				log.Printf("server %s connect fail\n", server.Host)
+				return
+			}
+			defer sshClient.Close()
+
+			// create ssh session
+			session, err := sshClient.NewSession()
+			if err != nil {
+				log.Printf("server %s connect fail\n", server.Host)
+				return
+			}
+			defer session.Close()
+
+			// exec nvidia-smi to get information of GPU
+			combo, err := session.CombinedOutput("nvidia-smi --query-gpu=memory.used,memory.total --format=csv,noheader && nvidia-smi --query-compute-apps=gpu_name,gpu_bus_id,process_name,used_gpu_memory --format=csv,noheader")
+			if err != nil {
+				log.Printf("server %s exec nvidia-smi fail\n", server.Host)
+				return
+			}
+
+			fmt.Printf("machine:[%s]->memory used, memory total\n%s", server.Host, string(combo))
+			wg.Done()
+		}(server, port)
+	}
+
+	wg.Wait()
+	fmt.Println("Detection Job Done.")
 }
